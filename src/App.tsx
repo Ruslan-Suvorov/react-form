427b808f4fc974a72aec8b82bf83384b6f6ca600
Ruslan-Suvorov <bloodsrv@gmail.com>
2023-10-17 19:54:04 +0300

    add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import './App.css';
 
+interface RegForm {
+  userName: string;
+  age: number;
+}
+
 function App() {
+
+  const { register, formState: { errors, isValid }, handleSubmit, reset } = useForm<RegForm>({
+      mode: "onBlur",
+  });
+
+  const onSubmit: SubmitHandler<RegForm> = data => {
+    console.log(data);
+    reset()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Hook Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+        {...register("userName", {
+          required: "Введіть ім'я користувача!",
+          minLength: {
+            value: 5,
+            message: "Не менше 5 символів"
+          }
+        })}
+        />
+        <div style={{height: 40}}>
+          {errors?.userName && <p>{errors?.userName.message}</p>}
+        </div>
+        <input type="number"
+        {...register("age", {
+          required: "Введіть вік користувача!",
+          min: {
+            value: 18,
+            message: "Лише для повнолітніх!"
+          }
+        })}
+        />
+        <div style={{height: 40}}>
+          {errors?.age && <p>{errors?.age.message}</p>}
+        </div>
+        <input type="submit" disabled={!isValid}/>
+      </form>
     </div>
   );
 }
